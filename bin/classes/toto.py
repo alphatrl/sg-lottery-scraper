9b8746a9ff62cb46f58ed5f2871cc1ec261f4336
Amos Tan <alphatrl1997@gmail.com>
2018-04-22 13:14:39 +0800

    Add scrapping for toto results

---
+++ bin/classes/toto.py
@@ -0,0 +1,55 @@
+# import libraries
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
+
+class Toto:
+
+    # Init a Toto obj
+    def __init__(self):
+        self.date = None
+        self.drawNo = None
+        self.winningNo = []
+        self.additionalNo = None
+        self.region = None
+        self.url = None
+
+
+    # Set methods
+    def set_url(self, url):
+        self.url = url
+
+    def set_region(self, region):
+        self.region = region
+
+
+    ## Test variable
+    def test_variables(self):
+        print(self.date)
+        print(self.drawNo)
+        print(self.winningNo)
+        print(self.additionalNo)
+        print(self.region)
+        print(self.url)
+
+    # Scrap data from given url
+    def scrap_data(self):
+        fourD = urlopen(self.url)
+        soup = BeautifulSoup(fourD, "html.parser")
+
+        # Get date and convert to datetime
+        drawDate = soup.find(class_="drawDate").text
+        self.date = datetime.strptime(drawDate, "%a, %d %b %Y")
+
+        # Get the draw number end of the string through .split()
+        drawNumberString = (soup.find(class_="drawNumber").text).split(" ")
+        self.drawNo = drawNumberString[2]
+
+        # Get the Winning No. in a list
+        winningSoupList = soup.find_all(class_=re.compile("win"), limit=6)
+        for i in range (len(winningSoupList)):
+            self.winningNo.append(winningSoupList[i].text)
+
+        # Get Additional No.
+        self.additionalNo = soup.find(class_=re.compile("additional")).text
--- bin/scrapper.py
+++ bin/scrapper.py
@@ -1,12 +1,25 @@
 # import libraries
 from classes.fourD import FourD
+from classes.toto import Toto
 import os
 import settings
 from datetime import datetime
 
-fourD_URL = os.getenv("FOUR_D_SG_URL")
+# create 4D and ToTo objects
+fourD_URL = os.getenv("FOUR_D_URL")
+toto_URL = os.getenv("TOTO_URL")
 newFourD = FourD()
+newToTo = Toto()
+
+
+# set and load 4D obj data
 newFourD.set_url(fourD_URL)
 newFourD.set_region("SG")
 newFourD.scrap_data()
-newFourD.test_variables()
+# newFourD.test_variables()
+
+# set and load TOTO obj data
+newToTo.set_url(toto_URL)
+newToTo.set_region("SG")
+newToTo.scrap_data()
+# newToTo.test_variables()
