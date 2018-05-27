b5324da93c33c783eedad61eaecc83a50e7e626b
Amos Tan <alphatrl1997@gmail.com>
2018-05-27 17:05:07 +0800

    Get latest 4D result from server

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Main Application
+# Purpose: Get records from the database and returns as a JSON
+
 # import libraries
 import json
 import psycopg2
@@ -20,15 +23,25 @@ def datetime_handler(x):
 try:
     # connect to PostgreSQL database
     conn = psycopg2.connect(host=database_host,database=database_name)
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
     # panic out
 
-@app.route('/api/4d')
-def four_D():
- 
- cur = conn.cursor(cursor_factory=RealDictCursor)
+# pass a json containing all the 4D records
+@app.route('/api/4d/')
+def get_all_four_D():
  sql = "SELECT * FROM \"FourDTable\""
  cur.execute(sql)
  
  return json.dumps(cur.fetchall(), default=datetime_handler)
+
+# pass a JSON containing the latest 4D results
+@app.route('/api/4d/latest')
+def get_latest_four_D():
+ sql = """SELECT * FROM \"FourDTable\"
+            ORDER BY draw_number DESC"""
+ cur.execute(sql)
+ 
+ return json.dumps(cur.fetchone(), default=datetime_handler)
