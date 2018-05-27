cae061a38fe77dd77fa9c9b8e7371efc75f9a676
Amos Tan <alphatrl1997@gmail.com>
2018-05-27 22:52:12 +0800

    Update env

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from psycopg2.extras import RealDictCursor
 
 app = Flask(__name__)
 
-db_host = os.getenv("DATABASE_URL")
+db_host = os.getenv("DATABASE_HOST")
 db_name = os.getenv("DATABASE_NAME")
 db_user = os.getenv("DATABASE_USER")
 db_password = os.getenv("DATABASE_PASSWORD")
--- bin/scrapper.py
+++ bin/scrapper.py
@@ -24,7 +24,7 @@ newToTo.scrap_data(toto_URL)
 newToTo.test_variables()
 
 # TODO scrapper to postgresql
-db_host = os.getenv("DATABASE_URL")
+db_host = os.getenv("DATABASE_HOST")
 db_name = os.getenv("DATABASE_NAME")
 db_user = os.getenv("DATABASE_USER")
 db_password = os.getenv("DATABASE_PASSWORD")
