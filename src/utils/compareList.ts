7e71fb577e65c3045a34b3e2c645f15862bb4def
Amos Tan <alphatrl1997@gmail.com>
2021-12-14 21:50:36 +0800

    refactor: update comparision

--- src/utils/compareList.ts
+++ src/utils/compareList.ts
@@ -1,3 +1,4 @@
+import { isEqual } from 'lodash';
 /**
  * @param new_list lottery results retrieved from scrapping
  * @param old_list lottery results retrieved from server
@@ -22,16 +23,16 @@ export default function getListKeyDifference<T>(
     for (const key in new_list) {
       different_list.push(key);
     }
-  } else {
-    for (const key in new_list) {
-      if (JSON.stringify(new_list[key]) !== JSON.stringify(old_list[key])) {
-        console.log(`[${key}]: Fetched new data`);
-        different_list.push(key);
-      } else {
-        console.log(`[${key}]: No difference`);
-      }
-    }
+    return different_list;
   }
 
+  for (const key in new_list) {
+    if (isEqual(new_list[key], old_list[key])) {
+      console.log(`[${key}]: Fetched new data`);
+      different_list.push(key);
+    } else {
+      console.log(`[${key}]: No difference`);
+    }
+  }
   return different_list;
 }
