0cbbe56fe78c25f19b36ed6a64d3bb22c4d8f35d
Amos Tan <alphatrl1997@gmail.com>
2021-12-22 11:28:09 +0800

    feat: add Sentry

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/node';
 import dotenv from 'dotenv';
 import puppeteer, { Browser } from 'puppeteer';
 
@@ -11,17 +12,27 @@ import { writeStore } from './utils/output';
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === 'production';
-const isTesting = process.env.NODE_ENV === 'testing';
+const { NODE_ENV, SENTRY_DSN, SERVER_URL } = process.env;
+
+if (SENTRY_DSN) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+  });
+}
+
+const isProduction = NODE_ENV === 'production';
+const isTesting = NODE_ENV === 'testing';
 const notificationList = [];
 
 async function writeServerFile<T>(fileName: string): Promise<void> {
-  const url = `${process.env.SERVER_URL}/${fileName}`;
+  const url = `${SERVER_URL}/${fileName}`;
   console.log(`Retrieving ${fileName}.json from ${url}`);
-  const list = await getJSON<T>(url).catch(() => {
+  const list = await getJSON<T>(url).catch((error) => {
     console.error(`[ERROR]: \'${fileName}\` does not exists in server`);
+    Sentry?.captureException(error);
     return {};
   });
+
   Object.keys(list).length > 0
     ? writeStore(fileName, list)
     : console.warn('[WARN]: Skipping file creation');
@@ -47,6 +58,7 @@ async function processSingapore(browser: Browser) {
     );
   } catch (error) {
     console.error(error);
+    Sentry?.captureException(error);
   }
 }
 
@@ -84,7 +96,8 @@ const main = async () => {
 
 main()
   .then(() => process.exit(0))
-  .catch((e) => {
-    console.error(e);
+  .catch((error) => {
+    console.error(error);
+    Sentry?.captureException(error);
     process.exit(1);
   });
