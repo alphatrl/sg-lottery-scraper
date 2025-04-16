2aa204ae4dea09b22bb4e763269a4ff37a3b35b5
Amos Tan <alphatrl1997@gmail.com>
2025-04-17 00:31:43 +0800

    fix(network): Replace `node-fetch` polyfill

--- src/utils/fetch.js
+++
@@ -1,6 +0,0 @@
-const _importDynamic = new Function('modulePath', 'return import(modulePath)');
-
-export async function fetch(...args) {
-  const { default: fetch } = await _importDynamic('node-fetch');
-  return fetch(...args);
-}
