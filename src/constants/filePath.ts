1e3369a816125e4b2627eb92c4f938adf0f04e5f
Amos Tan <alphatrl1997@gmail.com>
2025-05-10 00:58:06 +0800

    fix(dir): Save to wrong directory

--- src/constants/filePath.ts
+++ src/constants/filePath.ts
@@ -5,4 +5,4 @@ export const DATA_DIR = path.join(process.cwd(), 'data');
 export const UPLOAD_DIR = path.join(process.cwd(), 'output');
 
 // Backwards compatibility with huat-mobile v1/v2
-export const HUAT_V2_COMPAT_DATA_DIR = path.join(UPLOAD_DIR, 'v1');
+export const HUAT_V2_COMPAT_DATA_DIR = path.join(TEMP_DIR, 'v1');
