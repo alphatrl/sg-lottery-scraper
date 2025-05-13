6b6e077ed886d1cd59b4f3467254bc12968b8528
Amos Tan <hello@amostan.me>
2025-05-13 23:41:11 +0800

    chore(ci): Allow `scrape-test` to skip notification checks (#48)
    * fix: Allowing skipping of notifications
    
    * chore(ci): Update `scrape-test`

--- src/constants/featureFlags.ts
+++ src/constants/featureFlags.ts
@@ -5,4 +5,6 @@ dotenv.config();
 export const featureFlags = {
   IS_CI: process.env.IS_CI === 'true',
   IS_PRODUCTION: process.env.NODE_ENV === 'production',
+  IS_NOTIFICATION_COMPARE_SKIPPED:
+    process.env.SKIP_NOTIFICATION_CHECK === 'true',
 };
